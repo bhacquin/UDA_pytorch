51facf03f1cdcac6434a7c83e9e441bdb3cdca85
bhacquin <bastien.van-delft@unchartech.com>
2019-09-19 11:44:50 +0200

    First commmit.

---
+++ install_requirements.sh
@@ -0,0 +1,7 @@
+pip uninstall tensorflow
+pip uninstall tensorboard
+pip install -q --ignore-installed tf-nightly-2.0-preview
+pip install tensorflow
+git clone https://github.com/bhacquin/pytorch_bert_addons.git
+pip install pytorch_bert_addons/pytorch-pretrained-BERT/.
+pip install -r requirements.txt
---
+++ run_uda.sh
@@ -0,0 +1,13 @@
+#pip uninstall tensorflow
+#pip uninstall tensorboard
+#pip install -q --ignore-installed tf-nightly-2.0-preview
+#pip install tensorflow
+#git clone https://github.com/bhacquin/pytorch_bert_addons.git
+#pip install pytorch_bert_addons/pytorch-pretrained-BERT/.
+
+#pip install tensorboardX
+#pwd
+rm -r logs
+mkdir -p logs
+tensorboard --host 0.0.0.0 --logdir=logs  &
+python run_uda.py --sequence_length 512 --multi_gpu  
